a39e16ae315e06012d35b0596f718752f0bd1aa7
talves <tony@3alves.com>
2022-11-10 15:04:30 -0800

    init: 🔥 netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import solid from "solid-start/vite";
 import { defineConfig } from "vite";
+import netlify from "solid-start-netlify";
+
 export default defineConfig({
-  plugins: [solid({ ssr: false })],
+  plugins: [solid({ adapter: netlify({ edge: false }) })],
 });
